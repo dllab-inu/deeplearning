025f818c61aeffd88eb9305e7bcd7249fefb86a1
an-seunghwan <dpeltms79@gmail.com>
2026-02-20 15:40:56 +0900

    update

--- 10_cnn.py
+++ 10_cnn.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 torch.manual_seed(42)
 #%%
+### 1D convolution
 conv = nn.Conv1d(in_channels=1, out_channels=1, kernel_size=2, bias=False)
 x = torch.randn(size=(1, 1, 4)) # [batch_size, in_channels, D]
 output = conv(x)
@@ -14,6 +15,7 @@ assert torch.equal(output[0, 0, 2], (x[0, :, [2, 3]] * conv.weight[0, ]).sum())
 print(conv.weight.shape) # [out_channels, in_channels, kernel_size]
 print(output.shape) # [batch_size, out_channels, D']
 #%%
+### 2D convolution
 conv = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=2, bias=False)
 x = torch.randn(size=(1, 1, 3, 4)) # [batch_size, in_channels, H, W]
 output = conv(x)
@@ -21,6 +23,7 @@ output = conv(x)
 print(conv.weight.shape) # [out_channels, in_channels, kernel_size, kernel_size]
 print(output.shape) # [batch_size, out_channels, H', W']
 #%%
+### 1D zero padding
 conv = nn.Conv1d(
     in_channels=1, out_channels=1, kernel_size=2, 
     padding='same', padding_mode='zeros')
@@ -30,6 +33,7 @@ output = conv(x)
 assert x.shape == output.shape
 print(output.shape) # [batch_size, out_channels, D]
 #%%
+### 2D zero padding
 conv = nn.Conv2d(
     in_channels=1, out_channels=1, kernel_size=2, 
     padding='same', padding_mode='zeros')
@@ -37,5 +41,68 @@ x = torch.randn(size=(1, 1, 3, 4)) # [batch_size, in_channels, H, W]
 output = conv(x)
 
 assert x.shape == output.shape
+print(output.shape) # [batch_size, out_channels, H, W]
+#%%
+### strided convolution
+conv = nn.Conv2d(
+    in_channels=1, out_channels=1, kernel_size=2, 
+    stride=2)
+x = torch.randn(size=(1, 1, 4, 4))
+output = conv(x)
+
+print(output.shape)
+#%%
+### 1D convolution with Multiple channels
+conv = nn.Conv1d(
+    in_channels=5, out_channels=1, kernel_size=2)
+x = torch.randn(size=(1, 5, 4)) # [batch_size, in_channels, D]
+output = conv(x)
+
+print(conv.weight.shape) # [out_channels, in_channels, kernel_size]
+print(output.shape) # [batch_size, out_channels, D']
+#%%
+### 2D convolution with Multiple channels
+conv = nn.Conv2d(
+    in_channels=5, out_channels=1, kernel_size=2)
+x = torch.randn(size=(1, 5, 3, 4)) # [batch_size, in_channels, H, W]
+output = conv(x)
+
+print(conv.weight.shape) # [out_channels, in_channels, kernel_size, kernel_size]
+print(output.shape) # [batch_size, out_channels, H', W']
+#%%
+### 1D convolution with multiple outputs
+conv = nn.Conv1d(
+    in_channels=5, out_channels=16, kernel_size=2)
+x = torch.randn(size=(1, 5, 4)) # [batch_size, in_channels, D]
+output = conv(x)
+
+print(conv.weight.shape) # [out_channels, in_channels, kernel_size]
+print(output.shape) # [batch_size, out_channels, D']
+#%%
+### 2D convolution with multiple outputs
+conv = nn.Conv2d(
+    in_channels=5, out_channels=16, kernel_size=2)
+x = torch.randn(size=(1, 5, 3, 4)) # [batch_size, in_channels, H, W]
+output = conv(x)
+
+print(conv.weight.shape) # [out_channels, in_channels, kernel_size, kernel_size]
+print(output.shape) # [batch_size, out_channels, H', W']
+#%%
+### 1D 1x1 convolution
+conv = nn.Conv1d(
+    in_channels=5, out_channels=16, kernel_size=1)
+x = torch.randn(size=(1, 5, 4)) # [batch_size, in_channels, D]
+output = conv(x)
+
+print(conv.weight.shape) # [out_channels, in_channels, kernel_size]
+print(output.shape) # [batch_size, out_channels, D]
+#%%
+### 2D 1x1 convolution
+conv = nn.Conv2d(
+    in_channels=5, out_channels=16, kernel_size=1)
+x = torch.randn(size=(1, 5, 3, 4)) # [batch_size, in_channels, H, W]
+output = conv(x)
+
+print(conv.weight.shape) # [out_channels, in_channels, kernel_size, kernel_size]
 print(output.shape) # [batch_size, out_channels, H, W]
 #%%
